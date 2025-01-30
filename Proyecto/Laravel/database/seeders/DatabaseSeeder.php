85ff109bc2c5f7116b85891e779f03f346e6e151
mp6k <cuentaclasedaw@gmail.com>
2025-01-30 12:28:19 +0100

    Commit de la seed de productos

--- Proyecto/Laravel/database/seeders/DatabaseSeeder.php
+++ Proyecto/Laravel/database/seeders/DatabaseSeeder.php
@@ -15,13 +15,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+     if(!User::where('email','test@example.com')->exists()){
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+     }   
+        
         // Llamar al seeder de productos
         $this->call([
-            ProductSeeder::class, // Añade esta línea para registrar el ProductSeeder
+            ProductSeeder::class,
         ]);
     }
 }
--- Proyecto/Laravel/database/seeders/ProductSeeder.php
+++ Proyecto/Laravel/database/seeders/ProductSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-
+use Illuminate\Support\Facades\File;
 
 class ProductSeeder extends Seeder
 {
@@ -15,10 +15,19 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
+        $jsonPath=database_path('data/products.json');
+        if(!File::exists($jsonPath)){
+            $this->command->error("No se ha encontrado el archivo.");
+            return;
+        }
          // Lee el archivo JSON desde database/data/products.json
-         $json = Storage::get('database/data/products.json');
-         $products = json_decode($json, true);
- 
+         $jsonContent = File::get($jsonPath);
+         $products = json_decode($jsonContent, true);
+        
+         if ($products === null) {
+            $this->command->error("El archivo JSON está vacío o tiene un fallo.");
+            return;
+        }
          // Inserta los datos en la tabla 'productos'
          DB::table('productos')->insert($products);
     }
