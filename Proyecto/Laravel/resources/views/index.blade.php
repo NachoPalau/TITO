218a012bf701d9112ed2542ef82b35c7c09e72ae
Kollin <carlosdelgadocampos231205@gmail.com>
2025-02-03 12:23:27 +0100

    Blades terminados de navbar y footer

--- Proyecto/Laravel/resources/views/index.blade.php
+++ Proyecto/Laravel/resources/views/index.blade.php
@@ -9,33 +9,9 @@
 </head>
 
 <body>
-    <header class="container-fluid bg-white border-bottom py-4 sticky-top">
-        <div class="row align-items-center">
-            <!-- Columna del logo (izquierda) -->
-            <div class="col-4 d-flex justify-content-start" style="padding-left:3%">
-                <img src="img_Header/logo.png" style="width: 80px;">
-            </div>
+    @include('layouts.navigation')
 
-            <!-- Columna del buscador (centrado) -->
-            <div class="col-4 d-flex justify-content-center">
-                <input type="text" class="form-control w-100" placeholder="Buscar">
-            </div>
-
-            <!-- Columna del login y carrito (derecha) -->
-            <div class="col-4 d-flex justify-content-end align-items-center gap-3" style="padding-right:3%">
-                <img class="img-fluid" src="./img_Header/login.jpg" alt="Login" style="width: 40px; height: 40px;">
-                <img class="img-fluid" src="./img_Header/carrito.png" alt="Carrito" style="width: 40px; height: 40px;">
-            </div>
-        </div>
-    </header>
-
-    <nav class="container-fluid bg-light py-4">
-        <div class="row justify-content-center">
-            <div class="col-auto"><a href="{{ route('productos') }}" class="nav-link active">PRODUCTOS</a></div>
-            <div class="col-auto"><a href="#" class="nav-link">RECETAS</a></div>
-            <div class="col-auto"><a href="#" class="nav-link">EVENTOS</a></div>
-        </div>
-    </nav>
+    @include('layouts.subnavbar')
     
     <div class="container my-4">
       
@@ -68,17 +44,7 @@
         </section>
     </div>
     
-    <footer class="container-fluid  text-white text-center py-4 mt-5" style="background-color: rgb(131, 20, 20);">
-        <p>Añadir correo para recibir notificaciones</p>
-        <div class="row justify-content-center">
-            <div class="col-auto">
-                <p class="m-0">TITO o LOGO</p>
-            </div>
-            <div class="col-auto"><a href="#" class="text-white">Contactanos</a></div>
-            <div class="col-auto"><a href="#" class="text-white">Términos y condiciones</a></div>
-            <div class="col-auto"><a href="#" class="text-white">Cookies</a></div>
-        </div>
-    </footer>
+    @include('layouts.footer')
 
 </body>
 </html>
---
+++ Proyecto/Laravel/resources/views/layouts/subnavbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="container-fluid bg-light py-4">
+        <div class="row justify-content-center">
+            <div class="col-auto"><a href="{{ route('productos') }}" class="nav-link active">PRODUCTOS</a></div>
+            <div class="col-auto"><a href="#" class="nav-link">RECETAS</a></div>
+            <div class="col-auto"><a href="#" class="nav-link">EVENTOS</a></div>
+        </div>
+</nav>
