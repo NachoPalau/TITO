40fed08ffd7c70340cedfb98c2e2cb85c0e6e58d
nachopalau <nacho.palau3001@gmail.com>
2025-02-06 13:37:44 +0100

    diseo de mispedidos

--- Proyecto/Laravel/resources/views/pedido/misPedidos.blade.php
+++ Proyecto/Laravel/resources/views/pedido/misPedidos.blade.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mis pedidos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
 </head>
 <body>
-    <h1>Mis pedidos</h1>
+
+@include('layouts.navigation')
+<br><br>    
+<h1>Mis pedidos</h1>
     @if($pedidos->isEmpty())
         <p>No tienes pedidos registrados</p>
     @else 
@@ -29,5 +35,9 @@
             </tbody>
         </table>
     @endif
+    <br><br><br><br><br><br><br><br><br>
+    <div style="position: sticky; top:0px">
+    @include('layouts.footer')
+    </div>
 </body>
 </html>
