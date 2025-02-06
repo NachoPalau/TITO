dd8bb911eeb9b24cd869b9a818967832cdb31d3c
Kollin <carlosdelgadocampos231205@gmail.com>
2025-02-06 12:59:49 +0100

    implementacion carrito con contenido

--- Proyecto/Laravel/resources/views/carrito.blade.php
+++ Proyecto/Laravel/resources/views/carrito.blade.php
@@ -8,23 +8,6 @@
     let overlay = document.getElementById("overlay");
     let body = document.body;
 
-    let productos = []; // Modificar porque se cogera del usuario
-
-    function actualizarCarrito() {
-        let sinContenidoCarrito = document.getElementById("sinContenidoCarrito");
-        let contenidoCarrito = document.getElementById("contenidoCarrito");
-
-        if (productos.length === 0) {
-            sinContenidoCarrito.style.display = "flex";
-            contenidoCarrito.style.display = "none";
-        } else {
-            sinContenidoCarrito.style.display = "none";
-            contenidoCarrito.style.display = "flex";
-        }
-    }
-
-    actualizarCarrito();
-
     carritoIcon.addEventListener("click", function() {
         slideCarrito.classList.add("slide-abierto");
         overlay.style.display = "block";
@@ -50,12 +33,24 @@
 <div id="slideCarrito" class="slide-carrito">
         <img class="img-fluid" src="{{ asset('img/carrito/Close.png') }}" id="cerrarCarrito" alt="close" style="width: 50px; margin-top:10px; ">
         <h4 style="margin-left:15px; margin-top:10px;"><b>CARRITO</b></h4>
+        @if(Auth::check())
+        @php
+            $carrito = json_decode(Auth::user()->carrito, true);
+        @endphp
+        @if (empty($carrito))
         <div id="sinContenidoCarrito">
             <img class="img-fluid" src="{{ asset('img/carrito/Shopping Cart.png') }}" alt="carritoRojo" style="width: 100px; ">
             <strong>Tu carrito está vacío</strong>
         </div>
+        @else
         <div id="contenidoCarrito">
             <p>Tu carrito tiene articulos</p>
         </div>
-
+        @endif
+        @else
+        <div id="sinContenidoCarrito">
+            <img class="img-fluid" src="{{ asset('img/carrito/Shopping Cart.png') }}" alt="carritoRojo" style="width: 100px; ">
+            <strong>Tu carrito está vacío</strong>
+        </div>
+        @endif
     </div>
