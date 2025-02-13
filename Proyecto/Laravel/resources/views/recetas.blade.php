ec89577545ef2a1f1b9af8a2897d702a13feea03
FranciscoTRAK <127100142+FranciscoTrak24@users.noreply.github.com>
2025-02-13 02:44:42 +0100

    fix 2

--- Proyecto/Laravel/resources/views/recetas.blade.php
+++ Proyecto/Laravel/resources/views/recetas.blade.php
@@ -9,30 +9,36 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
-
 <body>
 
 @include('layouts.navigation')
-<div class="container my-4">
-    <section>
-<div id="productosDes">
-        @foreach($recetasMas as $receta)
-        <div class="receta"  data-id="{{ $receta->id }}" data-guardados="{{ $receta->guardados }}">
-            <strong>{{ $receta->titulo }}</strong>
-            <img id="estrella" data-id="{{ $receta->id }}" src="{{ asset('img/carrito/estrellaVacia.svg') }}">
-            <p><strong>Descripción:</strong> {{ $receta->descripcion }}</p>
-            <p><strong>Ingredientes:</strong> {{ implode(', ', json_decode($receta->ingredientes, true)) }}</p>
-            <p><strong>Creador:</strong> {{ $receta->usuario->name ?? 'Desconocido' }}</p>
-                    <div class="flex items-center justify-end mt-4">
-                        <x-primary-button class="button-primary agregar-carrito">
-                            {{ __('Añadir al carrito') }}
-                            <img src="{{ asset('img/carrito/carrito.svg') }}" id="carrito">
-                        </x-primary-button>
-                    </div>
+
+
+<!-- MAIN CONTAINER -->
+<div class="container">
+    <h1>Recetas destacadas</h1>
+    <!-- NOVEDADES -->
+    <section class="novedades my-4">
+        <div id="productosDes">
+            @foreach($recetasMas as $receta)
+            <div class="receta" data-id="{{ $receta->id }}" data-precio="{{ $receta->precio_total ?? 0 }}">
+                <strong>{{ $receta->titulo }}</strong>
+                <img id="estrella" data-id="{{ $receta->id }}" src="{{ asset('img/carrito/estrellaVacia.svg') }}">
+                <p><strong>Descripción:</strong> {{ $receta->descripcion }}</p>
+                <p><strong>Ingredientes:</strong> {{ implode(', ', json_decode($receta->ingredientes, true)) }}</p>
+                <p><strong>Creador:</strong> {{ $receta->usuario->name ?? 'Desconocido' }}</p>
+                <p class="precio-receta"><strong>Precio:</strong> {{ $receta->precio_total ?? 0 }}€</p>
+                <div class="flex items-center justify-end mt-4">
+                    <x-primary-button class="button-primary agregar-carrito">
+                        {{ __('Añadir al carrito') }}
+                        <img src="{{ asset('img/carrito/carrito.svg') }}" id="carrito">
+                    </x-primary-button>
+                </div>
+            </div>
+            @endforeach
         </div>
-        @endforeach
-        </div>
-</section>
+    </section>
+
     <!-- FILTROS -->
     @include('components.filtrar')
 
@@ -74,7 +80,7 @@
 </div>
 <!-- FOOTER -->
 @include('layouts.footer')
-</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 <script>
@@ -251,23 +257,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (mostrandoFavoritos) {
                     receta.style.display = recetaFavorita ? "block" : "none";
                 } else {
-                    const recetaId = parseInt(this.closest('.receta').dataset.id);
-                    toggleFavorito(recetaId);
+                    receta.style.display = "block"; // Mostrar todas las recetas
                 }
-            });
+            }
         });
-        // Código para mostrar favoritos
-        const mostrarFavoritosBtn = document.getElementById("mostrarFavoritosBtn");
-        let mostrandoFavoritos = false; // Variable para controlar el estado
 
-        mostrarFavoritosBtn.addEventListener("click", function () {
-            mostrandoFavoritos = !mostrandoFavoritos; // Cambiar el estado
+        // Cambiar el texto del botón
+        mostrarFavoritosBtn.textContent = mostrandoFavoritos ? "Mostrar Todas las Recetas" : "Mostrar Recetas Favoritas";
+    });
 
-            const recetas = document.querySelectorAll("#recetas .receta");
-            recetas.forEach(receta => {
-                const estrella = receta.querySelector("img#estrella");
-                if (estrella) { // Verifica si la estrella existe
-                    const recetaFavorita = estrella.src.includes("estrella.svg"); // Verifica si la estrella está marcada
+    const ordenarAZBtn = document.querySelector('.filtros .btn:first-child'); // Botón A-Z
+    let ordenAscendente = true; // Variable para controlar el orden
 
     ordenarAZBtn.addEventListener("click", function () {
         ordenAscendente = !ordenAscendente; // Cambiar el orden
@@ -354,125 +354,23 @@ document.addEventListener("DOMContentLoaded", function () {
                     if (!response.ok) {
                         return response.json().then(err => {throw new Error(err.message)}); // Lanza error si la respuesta no es ok
                     }
-                }
-            });
+                    console.log('Favoritos guardados:', response);
+                })
+                .catch(error => {
+                    console.error('Error al guardar favoritos:', error);
+                    // Aquí puedes agregar código para manejar el error, por ejemplo, mostrar un mensaje al usuario.
+                });
 
-            // Cambiar el texto del botón
-            mostrarFavoritosBtn.textContent = mostrandoFavoritos ? "Mostrar Todas las Recetas" : "Mostrar Recetas Favoritas";
-        });
+            } catch (error) {
+                console.error("Error al parsear la cookie de favoritos:", error);
+                // Manejar el error de parseo de la cookie
+            }
 
-        const ordenarAZBtn = document.querySelector('.filtros .btn:first-child'); // Botón A-Z
-        let ordenAscendente = true; // Variable para controlar el orden
-
-        ordenarAZBtn.addEventListener("click", function () {
-            ordenAscendente = !ordenAscendente; // Cambiar el orden
-
-            const recetas = Array.from(document.querySelectorAll("#recetas .receta")); // Convertir a array
-
-            recetas.sort((a, b) => {
-                const tituloA = a.querySelector("strong").textContent.toLowerCase();
-                const tituloB = b.querySelector("strong").textContent.toLowerCase();
-                return ordenAscendente ? tituloA.localeCompare(tituloB) : tituloB.localeCompare(tituloA);
-            });
-
-            const contenedorRecetas = document.getElementById("recetas");
-            contenedorRecetas.innerHTML = ""; // Limpiar el contenedor
-
-            recetas.forEach(receta => {
-                contenedorRecetas.appendChild(receta); // Agregar las recetas ordenadas
-            });
-
-            // Cambiar el texto del botón
-            ordenarAZBtn.textContent = ordenAscendente ? "A-Z ⬆" : "Z-A ⬇";
-        });
-
-        const ordenarPrecioBtn = document.querySelector('.filtros .btn:nth-child(2)'); // Botón de precio
-        let ordenPrecioAscendente = true;
-
-        ordenarPrecioBtn.addEventListener("click", function () {
-            ordenPrecioAscendente = !ordenPrecioAscendente;
-
-            const recetas = Array.from(document.querySelectorAll("#recetas .receta"));
-
-            recetas.sort((a, b) => {
-                const precioA = parseFloat(a.dataset.precio);
-                const precioB = parseFloat(b.dataset.precio);
-
-                if (isNaN(precioA) || isNaN(precioB)) {
-                    // Manejar casos donde el precio no es un número válido
-                    return 0; // O alguna otra lógica que consideres apropiada
-                }
-
-                return ordenPrecioAscendente ? precioA - precioB : precioB - precioA;
-            });
-
-            const contenedorRecetas = document.getElementById("recetas");
-            contenedorRecetas.innerHTML = "";
-
-            recetas.forEach(receta => {
-                contenedorRecetas.appendChild(receta);
-            });
-
-            ordenarPrecioBtn.textContent = ordenPrecioAscendente ? "Precio ⬆" : "Precio ⬇";
-        });
-
-    });
-
-
-    document.addEventListener('DOMContentLoaded', function () {
-        let csrfToken = null;
-        const metaTag = document.querySelector('meta[name="csrf-token"]');
-        if (metaTag) {
-            csrfToken = metaTag.getAttribute('content');
         } else {
-            console.error("No se encontró el token CSRF");
+            console.log("No se encontró la cookie de favoritos.");
         }
-
-        document.addEventListener('visibilitychange', function (e) {
-            if (document.visibilityState === 'hidden' && csrfToken) {
-                guardarFavoritos(csrfToken);
-            }
-        });
-
-        function guardarFavoritos(token) {
-            // Obtener la cookie llamada 'favoritos'
-            const favoritosCookie = document.cookie.split('; ').find(row => row.startsWith('favoritos='));
-
-            // Verificar si la cookie existe
-            if (favoritosCookie) {
-                // Extraer el valor de la cookie y parsearlo como JSON
-                try {
-                    const favoritos = JSON.parse(favoritosCookie.split('=')[1]);
-
-                    fetch('/guardar-favoritos', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'X-CSRF-TOKEN': token
-                        },
-                        body: JSON.stringify({ favoritos: favoritos }) // Enviar los favoritos como JSON
-                    })
-                        .then(response => {
-                            if (!response.ok) {
-                                return response.json().then(err => { throw new Error(err.message) }); // Lanza error si la respuesta no es ok
-                            }
-                            console.log('Favoritos guardados:', response);
-                        })
-                        .catch(error => {
-                            console.error('Error al guardar favoritos:', error);
-                            // Aquí puedes agregar código para manejar el error, por ejemplo, mostrar un mensaje al usuario.
-                        });
-
-                } catch (error) {
-                    console.error("Error al parsear la cookie de favoritos:", error);
-                    // Manejar el error de parseo de la cookie
-                }
-
-            } else {
-                console.log("No se encontró la cookie de favoritos.");
-            }
-        }
-    });
+    }
+});
 </script>
 
 </html>
